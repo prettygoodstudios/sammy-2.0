d6a819aa188b26ab519448e762f26d4b4258e3b0
Miguel Rust <hava12soft@gmail.com>
2020-10-06 16:51:51 -0600

    Fixed addional issue with physics

--- src/physics/object.js
+++ src/physics/object.js
@@ -25,11 +25,13 @@ export default class PhysicalObject extends Geometry {
             if(this.leftCollision(g, Math.abs(this.velocityX)*0.5)){
                 if(this.velocityX > 0){
                     this.velocityX = 0;
+                    this.x = g.x-this.width;
                 }
             }
             if(this.rightCollision(g, Math.abs(this.velocityX)*0.5)){
                 if(this.velocityX < 0){
                     this.velocityX = 0;
+                    this.x = g.x+g.width;
                 }
             }
         });
